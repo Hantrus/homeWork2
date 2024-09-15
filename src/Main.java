e687577b92793e18aebab8382a8901ded9e314fd
Игорь <hantd3@gmail.com>
2024-09-15 21:14:00 +0300

    Обьявил переменные разных типов

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
         public static void main(String[] args) {
         System.out.println("Делаю ДЗ");
+            {
+byte dogs = 125;
+short cats = 546;
+int ants = 55235;
+long fish = 2324756783L;
+float mug = 1.1F;
+double bottle = 1.525;
+                System.out.println("Собак в деревне: "+ dogs + ", котов в городе: "+cats + ", муравьев в лесу: " +ants +", рыб в океане:" +fish+". А вместимость кружки: " +mug + " и бутылки: " +bottle);
+
+            }
     }
 }
